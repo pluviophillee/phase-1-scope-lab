5c2a9ada2962b69ce93c08ffed680eeb14bcd5bd
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-05-07 00:27:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newCustomer){
+    bestCustomer = newCustomer;
+}
+
+const leastFavoriteCustomer = "anyone"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "meh";
+}
